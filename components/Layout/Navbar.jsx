215338f7dae755c82900c03f5db2ced9b5808a17
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-03-19 03:20:57 +0600

    nav update

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -30,17 +30,20 @@ const Navbar = () => {
     <section>
 
       {/* pc design  */}
-      <div className='container mx-auto px-5 md:py-3 overflow-hidden md:px-0'>
+      <div className='container mx-auto px-5 md:py-3  md:px-0'>
         <div className='flex justify-between items-center'>
-          <div className='flex items-center gap-10'>
+          <div className='flex items-center gap-5 lg:gap-10 overflow-hidden'>
             <div className='flex items-center'>
               <IoMdMenu onClick={MenuHandel} className='text-4xl md:hidden' />
 
               <Image className='w-22 md:w-36' src={navLogo} alt='Nav Logo Image' />
 
             </div>
-            <div className='hidden md:flex md:text-[12px] lg:text-[14px] overflow-clip'>
-              <ul className='flex flex-col md:flex-row gap-3'>
+            <div className={`md:static md:h-fit md:bg-white  md:text-black md:duration-0 ${menubar ? 'absolute left-0 top-0 bg-black text-white w-[100%] h-[100%] ease-in-out duration-300 z-50' : ' absolute -left-70'}`}>
+              <div className='md:hidden text-4xl pt-5 pl-8' onClick={() => setMenuBar(false)}>
+                <RxCross1 className='' />
+              </div>
+              <ul className='flex flex-col justify-center items-center  md:flex-row gap-10 mt-10 md:gap-3 lg:gap-5 md:mt-0 text-[13px] md:text-[18]'>
                 <li><Link href="#">Home</Link></li>
                 <li>
                   <select className='outline-none' name="categories" id="">
@@ -61,10 +64,10 @@ const Navbar = () => {
           </div>
           <div className='flex gap-2 text-[18px] md:text-[20px]'>
             <div className=''>
-              {search ? <div className='hidden  max-w-[160px] md:flex items-center px-2 py-1 rounded-xl text-black border-[1px] border-[#77d330]'>
-                <input className='outline-none px-1 py-1 text-[12px]' type="text" name="" id="" placeholder='Search' />
+              {search ? <div className='max-w-[160px]  h-8 flex justify-between items-center px-2  rounded-xl text-black border-[1px] border-[#77d330]'>
+                <input className='outline-none bg-transparent  px-1 py-1 text-[12px] w-[85%] ' type="text" name="" id="" placeholder='Search' />
                 <IoSearchOutline />
-              </div> : <div className='py-2 hidden md:block'><IoSearchOutline  onClick={searchHandel} /></div>
+              </div> : <div className='py-2'><IoSearchOutline onClick={searchHandel} /></div>
 
               }
 
@@ -92,7 +95,7 @@ const Navbar = () => {
         </div>
 
         {/* mobile Design  */}
-        <div className={menubar ? ' fixed right-0 top-0 w-[100%] md:hidden h-screen bg-black text-white px-10 ease-in duration-700  ' : 'fixed right-[-100%] top-0 p-1  ease-in duration-500'}>
+        {/* <div className={menubar ? ' fixed right-0 top-0 w-[100%] md:hidden h-screen bg-black text-white px-10 ease-in duration-700  ' : 'fixed right-[-100%] top-0 p-1  ease-in duration-500'}>
           <div className='text-4xl pt-5' onClick={()=> setMenuBar(false)}>
               <RxCross1 className='' />
           </div>
@@ -148,7 +151,7 @@ const Navbar = () => {
             </div>
           </div>
 
-        </div>
+        </div> */}
 
 
 
