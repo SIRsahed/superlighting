925cb7a318f084c35534c69dc89ebb2b5801d3a4
iftykhar alam <s.m.ifty49@gmail.com>
2025-03-18 23:46:21 +0600

    pull try

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,4 @@
 import Banner from "@/components/Home/Banner";
-import Bestseller from "@/components/Home/Bestseller";
-import SaleBanner from "@/components/Home/SaleBanner";
 import Image from "next/image";
 
 export default function Home() {
