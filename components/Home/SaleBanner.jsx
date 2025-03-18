9cbb42af1493767eb7cbaaed9c490372b09ccc7e
iftykhar alam <s.m.ifty49@gmail.com>
2025-03-18 13:52:56 +0600

    SaleBanner intialized

---
+++ components/Home/SaleBanner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SaleBanner = () => {
+  return (
+    <section className='sale-banner container'>
+
+    <div className="text-center">
+        
+    </div>
+
+    </section>>
+  )
+}
+
+export default SaleBanner
