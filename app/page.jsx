62af412ed6f1b1d446d56efcdb9366083fbb56c8
iftykhar alam <s.m.ifty49@gmail.com>
2025-03-18 15:16:47 +0600

    salebanner content and button done

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Banner from "@/components/Home/Banner";
+import SaleBanner from "@/components/Home/SaleBanner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Banner/>
+      <SaleBanner />
     </>
   );
 }
--- components/Home/SaleBanner.jsx
+++ components/Home/SaleBanner.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 
 const SaleBanner = () => {
   return (
-    <section className='sale-banner container'>
+    
+    <section className='sale-banner container px-6'>
 
-    <div className="text-center">
-        
+    <div className="text-center mb-5">
+        <div className="">
+            <p className=' text-2xl'>Every Products For 7 Days</p>
+        </div>
+        <div className="text-4xl font-bold text-center mb-5">
+            <h2>Up To 50%</h2>
+            <h2>Off Final Sale Products</h2>
+        </div>
+        <div className="mt-5">
+            <button className=' px-3 py-2  bg-black text-white ' >SHOP NOW</button>
+        </div>
     </div>
+        
 
-    </section>>
+    </section>
   )
 }
 
