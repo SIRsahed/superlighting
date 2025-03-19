04119f607a077d0ec757c78f845385b6f7633d22
iftykhar alam <s.m.ifty49@gmail.com>
2025-03-19 14:36:32 +0600

    bestseller and salebanner

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,10 @@
 import Banner from "@/components/Home/Banner";
-import Bestseller from "@/components/Home/Bestseller";
-import SaleBanner from "@/components/Home/SaleBanner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Banner/>
-      <SaleBanner />
-      <Bestseller />
     </>
   );
 }
