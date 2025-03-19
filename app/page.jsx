82e013d59ca2f2a26141480e29965422ddd9b774
iftykhar alam <s.m.ifty49@gmail.com>
2025-03-19 17:33:36 +0600

    featured product done with mobile responsive

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,10 @@
 import Banner from "@/components/Home/Banner";
-import Bestseller from "@/components/Home/Bestseller";
-import FeaturedProducts from "@/components/Home/FeaturedProducts";
-import SaleBanner from "@/components/Home/SaleBanner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Banner/>
-      <FeaturedProducts />
-      <SaleBanner />
-      <Bestseller />
     </>
   );
 }
