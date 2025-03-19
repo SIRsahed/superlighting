633f5c2c8b354edb5d5eac0881b70c9c0bd0ec04
aklimaakter20 <aklimaakter20@gmail.com>
2025-03-19 13:09:10 +0600

    category and banner section some edited

--- components/Home/Banner.jsx
+++ components/Home/Banner.jsx
@@ -2,25 +2,23 @@ import React from "react";
 import Image from "next/image"
 import Banner1 from "../../assets/banner7.avif";
 
-
 const Banner = () => {
   return (
-    <section className=" relative h-[90vh] w-[100vw] object-cover -z-1 pt-10 lg:pt-20 overflow-x-hidden ">
+    <section className=" relative h-screen w-[100vw] object-cover -z-1 pt-10 lg:pt-20 overflow-x-hidden shadow-4xl mx-auto ">
       {/* banner-image */}
-      <Image alt="banner" src={Banner1} fill={true} />
+      <Image className="w-full h-auto " alt="banner" src={Banner1} fill={true}  />
       <div className="container absolute px-10 lg:px-20 ">
-        <div className="absolute bg-black h-[100vh] w-[100vw] -top-20 left-0 opacity-20"></div>
-        <div className="banner-content lg:max-w-[600px] max-w-[400px] bg-white p-6 lg:p-10 mt-16   ">
+        {/* <div className="absolute bg-black h-[100vh] w-[100vw] -top-20 left-0 opacity-20"></div> */}
+        <div className="banner-content lg:max-w-[550px] max-w-[400px] bg-white px-10 lg:px-20 py-6 lg:py-8 mt-16   ">
           <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold leading-snug text-[#2E2E2E]">
-            Minimal <span className="text-[#9DCA42]">Interior</span> Enhance{" "}
-            <span className="text-[#2BA14D]">Your House</span>{" "}
+            Super <span className="text-[#2BA14D]">Light</span> Super{" "}
+            <span className="text-[#afeb24]">Bright</span>{" "}
           </h1>
-          <p className="text-[#2E2E2E] lg:max-w-[500px] max-w-[400px] mt-3">
-            The smartest guide to decorating kitchen cabinets to help optimize
-            the area of ​​family kitchen space, improve the experience.{" "}
+          <p className="text-[#2E2E2E] lg:max-w-[500px] max-w-[400px] mt-4 text-sm md:text-md">
+          Super Light BD is a trusted lighting business providing a wide range of interior and outdoor lighting solutions. Illuminate your space with our high-quality and stylish lights.{" "}
           </p>
-          <button className="bg-[#22813E] mt-3 px-5 py-3 rounded-md text-white font-bold">
-            Shop Now
+          <button className="bg-[#2BA14D] mt-4 px-5 py-3 rounded-md text-white font-semibold  text-sm md:text-md">
+            Know More
           </button>
         </div>
       </div>
--- components/Home/Category.jsx
+++ components/Home/Category.jsx
@@ -1,51 +1,64 @@
 import React from 'react'
 import Image from "next/image"
-import Light1 from '../../assets/light1.jpg'
-import Light2 from '../../assets/light2.jpg'
-import Light3 from '../../assets/light3.jpg'
-import Light4 from '../../assets/light4.jpg'
+import HighBayLight from '../../assets/led-high-bay.jpg'
+import FloodLight from '../../assets/floodlight.jpg'
+import PanelLight from '../../assets/panel-light.jpg'
+import StreetLight from '../../assets/street-light.jpg'
 
 
 const Category = () => {
     const categoryList = [
         {
-          image: Light1,
-          title: "T8 LED Complete Tube Light",
-          power: "10w to 28w",
+            image: HighBayLight,
+            title: "LED High Bay Light-COB Type",
+            power: "50w to 200w",
         },
         {
-        image: Light2,
-          title: "LED Bulb - E27/B22",
-          power:"05w to 100w",
+            image: FloodLight,
+            title: "LED Flood Light-COB Type",
+            power:"10w to 400w",
         },
 
         {
-            image: Light3,
-            title: "T5 LED Complete Tube Light",
-            power:"05w to 20w",
+            image: PanelLight,
+            title: "LED Panel Light",
+            power:"03w to 48w",
           },
         {
-            image: Light4,
-          title: "CATA Shade Tube Light",
-          power:"05w to 20w",
+            image: StreetLight,
+            title: "LED Street Light-COB Typ",
+            power:" 50w to 200w",
         },
       
       ];
   return (
-    <section className='category pt-8 lg:pt-10 bg-white'>
+    <section className='category py-8 lg:py-12 bg-white'>
         <div className="container">
            
-            <h2 className='text-[#2e2e2e] text-2xl md:text-3xl font-bold px-5 lg:px-10'>Categories</h2>
-           
+          <div className='flex items-center justify-between px-20'>
+          <h2 className='text-[#2e2e2e] text-2xl md:text-3xl lg:text-4xl underline underline-offset-8 font-bold  '>Categories</h2>
+            {/* slider-icon */}
+           <div className='flex items-center gap-3'>
+           <svg className='bg-[#7DBF46] text-white ' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"  >
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+              </svg>
+           <svg className='hover:bg-[#7DBF46] hover:text-white   transition-all' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+            <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+              </svg>
+          </div>
+             
+           </div>
+
             {/* card-area */}
-            <div className="category-card pt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 items-center px-5 lg:px-10">
+            <div className="category-card py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 items-center px-10 ">
                 {categoryList.map((item,i) => (
                     <div key={i}>
-                    <div  className="card border-2 border-[#1B6732]">
-                    <Image src={item.image} alt={Light1}  style={{borderRadius: 5}}/>
-                    <div className='text-center bg-[#2e2e2e] text-white px-3 py-5  ' >
-                        <h6 className='text-sm'>{item.title}</h6>
-                        <p className='text-xs'>{item.power}</p>
+                    <div  className="card border-1 border-[#2e2e2e] group-hover:shadow-green-700 group-hover:shadow-5xl transition-all delay-500 ease-in-out hover:border-[#1B6732] group">
+                    <Image className='w-full h-auto' src={item.image}  style={{borderRadius: 5}}/>
+                    <div className='text-center  px-3 py-5 group-hover:bg-[#2e2e2e] text-[#1B6732] group-hover:text-white hover: border-t-1 hover:shadow-gray-600 transition-all ease-in-out delay-700' >
+                        <h6 className='text-sm '>{item.title}</h6>
+                        <p className='text-xs mt-1 '>{item.power}</p>
+
                     </div>
                 </div>
                 </div>
