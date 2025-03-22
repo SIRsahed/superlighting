d8a661ef45e67807114a7a260eb4bff76607b664
iftykhar alam <s.m.ifty49@gmail.com>
2025-03-22 17:38:43 +0600

    doing forms

--- components/Contactus/Forms.jsx
+++ components/Contactus/Forms.jsx
@@ -4,19 +4,51 @@ import { IoLocationOutline  } from "react-icons/io5";
 const Forms = () => {
   return (
     <section className="contact-form py-12">
-        <div className="container w-full mx-auto ">
+        <div className="container w-full mx-auto  flex">
             <div className="">
                 <div className="flex gap-4 items-center ">
                     <div className="icon  items-center ">
                     <IoLocationOutline size={45} />
                     </div>
-                    <div className="icon-words w-1/2">
+                    <div className="icon-words w-1/2 ">
+                        <h2 className='font-bold' >Store Address</h2>
+                        <p>House: 110, Road: 02, Block: D, Pallabi 2nd Project, Rupnagar, Mirpur</p>
+                    </div>
+                </div>
+                <div className="flex gap-4 items-center ">
+                    <div className="icon  items-center ">
+                    <IoLocationOutline size={45} />
+                    </div>
+                    <div className="icon-words w-1/2 ">
+                        <h2 className='font-bold' >Store Address</h2>
+                        <p>House: 110, Road: 02, Block: D, Pallabi 2nd Project, Rupnagar, Mirpur</p>
+                    </div>
+                </div>
+                <div className="flex gap-4 items-center ">
+                    <div className="icon  items-center ">
+                    <IoLocationOutline size={45} />
+                    </div>
+                    <div className="icon-words w-1/2 ">
+                        <h2 className='font-bold' >Store Address</h2>
+                        <p>House: 110, Road: 02, Block: D, Pallabi 2nd Project, Rupnagar, Mirpur</p>
+                    </div>
+                </div>
+                <div className="flex gap-4 items-center ">
+                    <div className="icon  items-center ">
+                    <IoLocationOutline size={45} />
+                    </div>
+                    <div className="icon-words w-1/2 ">
                         <h2 className='font-bold' >Store Address</h2>
                         <p>House: 110, Road: 02, Block: D, Pallabi 2nd Project, Rupnagar, Mirpur</p>
                     </div>
                 </div>
             </div>
-            <div className=""></div>
+            <div className="">
+                <form action="" method="post">
+                    <p className="text-2xl">We are Here To Help you</p>
+                    <input type="text" />
+                </form>
+            </div>
         </div>
     </section>
   )
