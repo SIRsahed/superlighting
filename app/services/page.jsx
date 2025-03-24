55b3858f7f7080aeeae38cdcadf82a6dc72e013f
aklimaakter20 <aklimaakter20@gmail.com>
2025-03-24 16:58:04 +0600

    service page er service one section  added

---
+++ app/services/page.jsx
@@ -0,0 +1,12 @@
+import Service1 from '@/components/service1'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+     <Service1/> 
+    </>
+  )
+}
+
+export default page
---
+++ components/Service2.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Image from "next/image"
+import ServiceImage1 from "@/public/ServiceImage1.avif";
+import ServiceImage2 from "@/public/ServiceImage2.avif";
+import ServiceImage3 from "@/public/ServiceImage3.avif";
+const Service2 = () => {
+  const serviceOne = [
+    {
+      image: ServiceImage1,
+      info : "Interior Lighting Solutions",
+    },
+    {
+      image: ServiceImage2,
+      info : "Interior Lighting Solutions",
+    },
+    {
+      image: ServiceImage3,
+      info : "Interior Lighting Solutions",
+    },
+]
+
+
+  return (
+    <section className='py-10'>
+      <div className="container mx-auto">
+        <h2 className='text-2xl md:text-3xl font-bold underline underline-offset-8 text-cen pb-10'>Our Services</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+          { serviceOne.map((item,index)=>(
+            <div key={index} className='card'>
+              <Image className='w-100 h-60 hover:scale-110 transition duration-700 mb-3 ' src={item.image} alt='ServiceImage1'/>
+              <h3>{item.info}</h3>
+            </div>
+          ))}
+         
+         
+        </div>
+      </div>
+      
+    </section>
+  )
+}
+
+export default Service2
+
