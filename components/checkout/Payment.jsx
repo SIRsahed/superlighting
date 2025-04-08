7b709c7d40f18d970a160200f874b70c68420d29
iftykhar alam <s.m.ifty49@gmail.com>
2025-04-09 02:35:38 +0600

    checkoutpage initiated

---
+++ components/checkout/Payment.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { GiTakeMyMoney } from "react-icons/gi";
+
+const Payment = () => {
+  return (
+    <section className='payment container w-full mx-auto py-12 px-4'>
+      {/* <div className="flex flex-col gap-8 items-center justify-center">
+        <div className="icon">
+          <GiTakeMyMoney size={45} />
+        </div>
+        <div className="icon-words w-full md:w-1/2 text-center">
+          <h2 className="font-bold text-2xl">Payment Method</h2>
+          <p>We accept all types of payment methods.</p>
+        </div>
+      </div> */}
+      
+    </section >
+  )
+}
+
+export default Payment
