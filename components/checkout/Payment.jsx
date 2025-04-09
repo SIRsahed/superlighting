af22219a1adb02b524855ab91e2fce1841174e96
iftykhar alam <s.m.ifty49@gmail.com>
2025-04-09 20:28:01 +0600

    checkbox and button added

--- components/checkout/Payment.jsx
+++ components/checkout/Payment.jsx
@@ -2,70 +2,137 @@ import React from 'react'
 import { GiTakeMyMoney } from "react-icons/gi";
 import { GiPaperPlane } from "react-icons/gi";
 import { SiFreelancer } from "react-icons/si";
+import { FaCcVisa } from "react-icons/fa";
+import { FaCcMastercard } from "react-icons/fa";
+import { SiAmericanexpress } from "react-icons/si";
 
 const Payment = () => {
   return (
-    <section className='payment container w-full mx-auto py-12 px-4'>
-      {/* <div className="flex flex-col gap-8 items-center justify-center">
-        <div className="icon">
-          <GiTakeMyMoney size={45} />
-        </div>
-        <div className="icon-words w-full md:w-1/2 text-center">
-          <h2 className="font-bold text-2xl">Payment Method</h2>
-          <p>We accept all types of payment methods.</p>
-        </div>
-      </div> */}
+    <section className="payment container w-full mx-auto py-12 px-4">
       <div className="container w-full mx-auto flex flex-col md:flex-row gap-8 items-center justify-center">
         <div className="w-full md:w-1/2 flex flex-col gap-6  ">
-            <form action="" method="post" className="w-full">
-                <div className="bg-gray-100 border-2 border-gray-100 py-6 px-6 md:px-10">
-                    
-                    <div className="p-4 ">
-                        <p className='text-2xl text-center md:text-left'>Payment Method<span className=''>(Please select a payment method)</span></p>
-                    </div>
-                    <hr className='text-gray-300' />
-                    <div className="cash-on-delivery  gap-4 items-center justify-center md:justify-start my-4 ">
-                        <div className=" py-6 ">
-                            <p className="text-xl">Pay After Delivary</p>
-                        </div>
-                        <div className=" flex items-center gap-2 bg-white p-4 border-gray-300 border-2 rounded-md">
-                            <input type="radio" name="payment" id="cash-on-delivery" value="cash-on-delivery" className='w-5 h-5' />
-                            <GiTakeMyMoney className='text-3xl' />
-                            <p>Cash On Delivary</p>
-                        </div>
-                    </div>
-                    <hr className='text-gray-300' />
-                    <div className="cash-on-delivery  gap-4 items-center justify-center md:justify-start my-4 ">
-                        <div className=" py-6 ">
-                            <p className="text-xl">Mobile Wallet/Banking</p>
-                        </div>
-                        <div className=" flex flex-col gap-4 md:flex-row items-center justify-center md:justify-start my-4 ">
-                            <div className=" flex items-center gap-2 bg-white p-4 border-gray-300 border-2 rounded-md">
-                                <input type="radio" name="payment" id="cash-on-delivery" value="cash-on-delivery" className='w-5 h-5' />
-                                <SiFreelancer className='text-3xl' />
-                                <p>bKash</p>
-                            </div>
-                            <div className=" flex items-center gap-2 bg-white p-4 border-gray-300 border-2 rounded-md">
-                                <input type="radio" name="payment" id="cash-on-delivery" value="cash-on-delivery" className='w-5 h-5' />
-                                <GiTakeMyMoney className='text-3xl' />
-                                <p>Nagad</p>
-                            </div>
-                            <div className=" flex items-center gap-2 bg-white p-4 border-gray-300 border-2 rounded-md">
-                                <input type="radio" name="payment" id="cash-on-delivery" value="cash-on-delivery" className='w-5 h-5' />
-                                <GiPaperPlane className='text-3xl' />
-                                <p>Rocket</p>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    
+          <form action="" method="post" className="w-full">
+            <div className="bg-gray-100 border-2 border-gray-100 py-6 px-6 md:px-10">
+              <div className="p-4 ">
+                <p className="text-2xl text-center md:text-left">
+                  Payment Method
+                  <span className="">(Please select a payment method)</span>
+                </p>
+              </div>
+
+              <hr className="text-gray-300" />
+              <div className="cash-on-delivery  gap-4 items-center justify-center md:justify-start my-4 ">
+                <div className=" py-6 ">
+                  <p className="text-xl">Pay After Delivary</p>
                 </div>
-            </form>
+                <div className=" flex items-center gap-2 bg-white p-4 border-gray-300 border-2 rounded-md">
+                  <input
+                    type="radio"
+                    name="payment"
+                    id="cash-on-delivery"
+                    value="cash-on-delivery"
+                    className="w-5 h-5"
+                  />
+                  <GiTakeMyMoney className="text-3xl" />
+                  <p>Cash On Delivary</p>
+                </div>
+              </div>
+
+              <hr className="text-gray-300" />
+              <div className="Mobile-wallet  gap-4 items-center justify-center md:justify-start my-4 ">
+                <div className=" py-6 ">
+                  <p className="text-xl">Mobile Wallet/Banking</p>
+                </div>
+                <div className=" flex flex-col sm:flex-col sm:justify-start gap-4 md:flex-row  justify-center md:justify-start my-4 ">
+                  <div className=" flex items-center gap-2 bg-white p-4 border-gray-300 border-2 rounded-md">
+                    <input
+                      type="radio"
+                      name="payment"
+                      id="cash-on-delivery"
+                      value="cash-on-delivery"
+                      className="w-5 h-5"
+                    />
+                    <SiFreelancer className="text-3xl bg-" />
+                    <p>bKash</p>
+                  </div>
+                  <div className=" flex items-center gap-2 bg-white p-4 border-gray-300 border-2 rounded-md">
+                    <input
+                      type="radio"
+                      name="payment"
+                      id="cash-on-delivery"
+                      value="cash-on-delivery"
+                      className="w-5 h-5"
+                    />
+                    <GiTakeMyMoney className="text-3xl" />
+                    <p>Nagad</p>
+                  </div>
+                  <div className=" flex items-center gap-2 bg-white p-4 border-gray-300 border-2 rounded-md">
+                    <input
+                      type="radio"
+                      name="payment"
+                      id="cash-on-delivery"
+                      value="cash-on-delivery"
+                      className="w-5 h-5"
+                    />
+                    <GiPaperPlane className="text-3xl" />
+                    <p>Rocket</p>
+                  </div>
+                </div>
+
+                <hr className="text-gray-300" />
+                <div className="card-payment  gap-4 items-center justify-center md:justify-start my-4 ">
+                  <div className=" py-6 ">
+                    <p className="text-xl">Pay With Dabit/Credit Card</p>
+                  </div>
+                  <div className=" flex items-center gap-2 bg-white p-4 border-gray-300 border-2 rounded-md gap-4 md:flex-row">
+                    <input
+                      type="radio"
+                      name="payment"
+                      id="cash-on-delivery"
+                      value="cash-on-delivery"
+                      className="w-5 h-5"
+                    />
+
+                    <FaCcVisa className="text-4xl text-blue-500" />
+                    <FaCcMastercard className="text-4xl text-blue-950" />
+                    <SiAmericanexpress className="text-4xl text-blue-500" />
+                  </div>
+                </div>
+
+                <hr className="text-gray-300" />
+                <div className="flex flex-col md:flex-row p-4 gap-4 items-center">
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="" className="text-sm text-gray-900">
+                      I have read and agree to the terms and conditions
+                    </label>
+                </div>
+                
+                <hr className="text-gray-300" />
+                {/* <div className="flex flex-col md:flex-row p-4 gap-4 items-center ">
+                  <button
+                    className="bg-black text-white p-3 w-full md:w-auto"
+                    type="submit"
+                  >
+                    Confirm Order
+                  </button>
+                </div> */}
+                <div className="flex items-center justify-center p-4 w-full">
+                    <button
+                        className="bg-green-500 rounded text-white px-6 py-3"
+                        type="submit"
+                    >
+                        Confirm Order
+                    </button>   
+                </div>
+
+
+              </div>
+            </div>
+          </form>
         </div>
       </div>
-      
-    </section >
-  )
+    </section>
+  );
 }
 
 export default Payment
