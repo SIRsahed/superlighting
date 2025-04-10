187b7a561b57fe0bb9410a8e7670c98cecf6f1ad
sirsahed <sahedrahmanshakil@gmail.com>
2025-04-10 21:48:20 +0600

    build done

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/Home/Banner";
-import Image from "next/image";
 import Bestseller from "@/components/Home/Bestseller";
 import Blog from "@/components/Home/Blog";
 import Category from "@/components/Home/Category";
 import ClientSay from "@/components/Home/ClientSay";
+import Collection from "@/components/Home/Collection";
+import Delivary from "@/components/Home/Delivary";
 import FeaturedProducts from "@/components/Home/FeaturedProducts";
 import HomeAbout from "@/components/Home/HomeAbout";
+import Products from "@/components/Home/Products";
 import SaleBanner from "@/components/Home/SaleBanner";
 
 
