d29b8fd037c48c93cd792f5e5bd2eb5ebc359c04
Aklima Akter <aklimaakter20@gmail.com>
2025-04-19 19:50:42 +0600

    service hero section added

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,10 +1,12 @@
 import Service1 from '@/components/services/Service1'
 import Service2 from '@/components/services/Service2'
+import ServiceHero from '@/components/services/ServiceHero'
 import React from 'react'
 
 const page = () => {
   return (
     <>
+    <ServiceHero/>
      <Service1/>
      <Service2/>
     </>
