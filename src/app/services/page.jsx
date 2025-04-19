4e5e8a1e8b79ab9f58bc3cd0a3d057b7e83b8b31
Aklima Akter <aklimaakter20@gmail.com>
2025-04-19 11:23:10 +0600

    service section part 2 added

---
+++ src/app/services/page.jsx
@@ -0,0 +1,14 @@
+import Service1 from '@/components/services/Service1'
+// import Service2 from '@/components/services/Service2'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+     <Service1/>
+     {/* <Service2/> */}
+    </>
+  )
+}
+
+export default page
